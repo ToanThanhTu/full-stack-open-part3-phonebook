c09dea790618fc9e110f1595876485b484b08d0b
Trevor <toantu3005@gmail.com>
2024-07-10 20:26:38 +1000

    feat: modify delete to use mongodb; add put function to handle update request; add error handling module and middleware

--- index.js
+++ index.js
@@ -11,29 +11,6 @@ const app = express()
 // getting Person mongoose schema
 const Person = require('./modules/person')
 
-let persons = [
-    // {
-    //     "id": "1",
-    //     "name": "Arto Hellas",
-    //     "number": "040-123456"
-    // },
-    // {
-    //     "id": "2",
-    //     "name": "Ada Lovelace",
-    //     "number": "39-44-5323523"
-    // },
-    // {
-    //     "id": "3",
-    //     "name": "Dan Abramov",
-    //     "number": "12-43-234345"
-    // },
-    // {
-    //     "id": "4",
-    //     "name": "Mary Poppendieck",
-    //     "number": "39-23-6423122"
-    // }
-]
-
 // use cors to allow requests from other origins
 app.use(cors())
 
@@ -90,24 +67,60 @@ app.get('/info', (request, response) => {
 })
 
 // GET person: use findById
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person
         .findById(request.params.id)
         .then(person => {
             response.json(person)
         })
-        .catch((error) => {
-            response.status(404).end()
+        .catch((error) => next(error))
+})
+
+// DELETE: delete a person by id
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
         })
+        .catch(error => next(error))
 })
 
-// DELETE: TO-DO
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
+// PUT: update a person with the same name
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
 
-    response.status(204).end()
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    // findByIdAndUpdate function which takes id and new person object
+    // { new: true } - return the updated person rather than the original
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
+
+// Define unknown endpoint
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// Define error handler middleware
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+// use unknown endpoint and error handler middleware
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 // use PORT environment variable
 const PORT = process.env.PORT
